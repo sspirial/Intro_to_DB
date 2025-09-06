6960b97665426fc05e7c590a2838d9963cc8844a
Munubi Emmanuel <sspirial@gmail.com>
2025-09-06 11:17:05 +0300

    Replace DESCRIBE statement with SELECT query to retrieve column details of the 'Books' table

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,2 @@
 -- Print full description of the 'books' table without using DESCRIBE or EXPLAIN
-DESCRIBE Books;
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = DATABASE();
